0a4404766fb3734c791cf3e191c99a2dc5a0c644
Bryan Mytko <bryanmytko@gmail.com>
2022-02-07 23:42:25 -0500

    Add User model

---
+++ src/models/user.ts
@@ -0,0 +1,22 @@
+import { Schema, model } from 'mongoose';
+
+interface User {
+  email: string,
+  password: string
+}
+
+const schema = new Schema<User>({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = model<User>('User', schema);
+
+export default User;
