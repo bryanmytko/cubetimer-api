134a66cde46ade222fdcf3b8e9a8b4308ac6dfa8
Bryan Mytko <bryanmytko@gmail.com>
2022-02-08 00:10:55 -0500

    Add auth#login

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -8,6 +8,8 @@ import User from '../models/user';
 const router = express.Router();
 const ROUNDS = 10;
 
+const invalidLogin = (res: Response) => res.status(400).json({ error: 'Invalid email/password' });
+
 router.post('/signup', async (req, res) => {
   const { email, password } = req.body;
   const hash = await bcrypt.hash(password, ROUNDS);
@@ -24,16 +26,21 @@ router.post('/signup', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-  // @TODO
-  // Find user
-  // const password = 'foo';
-  // const userPassword = 'foo';
-  //
-  // bcrypt.compare(password, userPassword, (error, match) => {
-    return res.status(200);
-  // });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).lean();
+
+  if(!user) return res.status(400);
+
+  bcrypt.compare(password, user.password, (error, match) => {
+    if(error) return res.status(500).json({ error });
+    if(match) return res.status(201).json({ token: generateToken(user) });
+
+    return invalidLogin(res);
+  });
 });
 
+/* @TODO need some kind of jwt verification middleware */
 router.get('verify-jwt', (req, res) => {
   res.status(200);
 });
