8ada47020079e0128b78ee81dfe8ef24482d7556
Bryan Mytko <bryanmytko@gmail.com>
2022-02-07 23:42:01 -0500

    Include auth route

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino';
+import mongoose, { ConnectOptions } from 'mongoose';
 
 import authRoute from './routes/auth';
 
 const app = express();
 
 const PORT = 3000;
+const MONGO_URL = 'mongodb://localhost/cubetimer';
+
 const logger = pino({
   level: 'info'
 });
+
+mongoose.connect(`${MONGO_URL}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  autoIndex: true
+} as ConnectOptions);
+
+const db = mongoose.connection;
 
 app.use(express.json());
 app.use(cors({ origin: '*' }));
@@ -17,6 +28,9 @@ app.use(cors({ origin: '*' }));
 app.use('/api/auth', authRoute);
 // app.use('/api/sessions', sessionsRoute);
 
+db.once('open', () => logger.info('Database connected.'));
+db.on('error', (error: Error) => logger.info(`Database error: ${error}`));
+
 app.listen(PORT, () => {
-  logger.info(`Server started on port ${PORT}`);
+  logger.info(`iServer started on port ${PORT}`);
 });
