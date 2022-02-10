ccf29250e9fe274eeae89d27fd3392b51435d030
Bryan Mytko <bryanmytko@gmail.com>
2022-02-10 08:33:47 -0500

    Add puzzle type

--- src/models/average.ts
+++ src/models/average.ts
@@ -1,11 +1,23 @@
 import { Schema, Types, model } from 'mongoose';
 
+enum PuzzleType {
+  Cube2x2 = '2x2',
+  Cube3x3 = '3x3',
+  Cube4x4 = '4x4',
+  Cube5x5 = '5x5',
+  Cube6x6 = '6x6',
+  Cube7x7 = '7x7',
+  Pyraminx = 'Pyraminx',
+  Megaminx = 'Megaminx'
+}
+
 interface Average {
   kind: string,
   fastest: number,
   slowest: number,
   calculated: number,
-  solves: Types.Array<string>
+  solves: Types.Array<string>,
+  puzzleType: PuzzleType
 }
 
 const schema = new Schema<Average>({
@@ -28,6 +40,11 @@ const schema = new Schema<Average>({
   solves: {
     type: [Number],
     required: true
+  },
+  puzzleType: {
+    type: String,
+    default: PuzzleType.Cube3x3,
+    enum: Object.values(PuzzleType)
   }
 });
 
