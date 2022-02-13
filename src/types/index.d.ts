7c7d245975010054afdd83502faac07ae1b321a6
Bryan Mytko <bryanmytko@gmail.com>
2022-02-13 07:42:04 -0500

    Add JwtPayload type; other types for middleware

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,12 +1,37 @@
 declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      TOKEN_SECRET: string;
+      MONGO_URL: string;
+    }
+  }
+  
   interface ENV {
+    TOKEN_SECRET: string | undefined;
     MONGO_URL: string | undefined;
-    SECRET_TOKEN: string | undefined;
+  }
+  
+  interface Config {
+    TOKEN_SECRET: string;
+    MONGO_URL: string;
   }
 
-  interface Config {
-    MONGO_URL: string;
-    SECRET_TOKEN: string;
+  interface JwtPayload {
+    data: string;
+  }
+
+  interface UserType {
+    _id: string
+  }
+
+  namespace Express {
+    interface Request {
+        user?: string
+    }
+
+    interface Headers {
+      authorization: string;
+    }
   }
 }
 
